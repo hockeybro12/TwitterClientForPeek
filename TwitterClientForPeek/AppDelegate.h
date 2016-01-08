06db805b1ed69f106000eb5a0f08bbf229f83861
Nikhil Mehta <nikhilmehta@nikhils-macbook-pro.local>
2016-01-08 15:30:27 -0800

    Initial Commit

---
+++ TwitterClientForPeek/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterClientForPeek
+//
+//  Created by Nikhil Mehta on 1/8/16.
+//  Copyright © 2016 MehtaiPhoneApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterClientForPeek/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterClientForPeek
+//
+//  Created by Nikhil Mehta on 1/8/16.
+//  Copyright © 2016 MehtaiPhoneApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
